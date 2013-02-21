deed63ddfef80c05039eaae261ce6d561bd23743
Norbert <norbert@linuxnetworks.de>
2013-02-21 15:04:21 +0100

    Implements catalog filter startid, levels-always and levels-only in catalog-filter plugin and updates arcavias.css

--- ext_autoload.php
+++ ext_autoload.php
@@ -7,6 +7,9 @@ return array(
 	'tx_arcavias_scheduler_abstract' => $extensionPath . 'Classes/Scheduler/Abstract.php',
 	'tx_arcavias_scheduler_admin' => $extensionPath . 'Classes/Scheduler/Admin.php',
 	'tx_arcavias_scheduler_catalog' => $extensionPath . 'Classes/Scheduler/Catalog.php',
+
+	'tx_arcavias_flexform_abstract' => $extensionPath . 'Classes/Flexform/Abstract.php',
+	'tx_arcavias_flexform_catalog' => $extensionPath . 'Classes/Flexform/Catalog.php',
 );
 
 ?>
