4f19e3a4e72ea7f8b5683f1cde5e0579c6e6067d
default.mw-preview.net <preview@zmw-prv01.metaways.net>
2013-02-25 00:41:20 +0100

    Revert "Fixes APC prefix and optimizes autoloader"
    This reverts commit 09eaa9ad2dc28b169a647679c7e10067a41df534.

--- Classes/Controller/Abstract.php
+++ Classes/Controller/Abstract.php
@@ -99,8 +99,7 @@ abstract class Tx_Arcavias_Controller_Abstract extends Tx_Extbase_MVC_Controller
 
 		$conf = new MW_Config_Array( array(), $configPaths );
 		if( function_exists( 'apc_store' ) === true && $this->_getExtConfig( 'useAPC', false ) == true ) {
-			$prefix = ( isset( $settings['apc']['prefix'] ) ? $settings['apc']['prefix'] : '' );
-			$conf = new MW_Config_Decorator_APC( $conf, $prefix );
+			$conf = new MW_Config_Decorator_APC( $conf, $conf->get( 'apc/prefix' ) );
 		}
 		$conf = new MW_Config_Decorator_MemoryCache( $conf, $settings );
 
@@ -246,7 +245,7 @@ abstract class Tx_Arcavias_Controller_Abstract extends Tx_Extbase_MVC_Controller
 
 			require_once $libPath . $ds . 'core' . $ds . 'MShop.php';
 
-			if( spl_autoload_register( 'MShop::autoload', true, true ) === false ) {
+			if( spl_autoload_register( 'MShop::autoload' ) === false ) {
 				throw new Exception( 'Unable to register Arcavias autoload method' );
 			}
 
