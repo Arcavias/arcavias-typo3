8aad3a87776a3589829733cc5280608d703bd618
default.mw-preview.net <preview@zmw-prv01.metaways.net>
2013-03-02 15:38:32 +0100

    Simplifies RealURL configuration for 40% better performance in list views

--- realurl_autoconf.php
+++ realurl_autoconf.php
@@ -1,41 +1,19 @@
 <?php
 
 /**
-* RealUrl Configuration for extension Arcavias
-*
-*/
+ * RealUrl Configuration for extension Arcavias
+ */
 
-$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['fixedPostVars']['_DEFAULT'][] = array(
-		'GETvar' => 'arc[f-catalog-id]',
-		'lookUpTable' => array(
-			'table' => 'mshop_text mt JOIN mshop_catalog_list cl ON cl.refid=mt.id JOIN mshop_text_type mtt ON mtt.id = mt.typeid',
-			'id_field' => 'parentid',
-			'alias_field' => 'content',
-			'addWhereClause' => " AND cl.domain='text' AND mt.status > 0 AND (mt.langid='{TSFE:config|config|language}' or mt.langid is null) AND mt.domain='catalog' AND mtt.domain='catalog' AND mtt.code='name'",
-			'useUniqueCache' => 1,
-			'useUniqueCache_conf' => array(
-				'strtolower' => 1,
-				'spaceCharacter' => '-',
-			),
-			'insertData' => 1,
-		),
-);
+// Use no caching for better performance of uncached pages
+//$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['init'] = array(
+//	'enableCHashCache' => true,
+//	'enableUrlDecodeCache' => true,
+//	'enableUrlEncodeCache' => true,
+//);
 
-$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['fixedPostVars']['_DEFAULT'][] = array(
-	'GETvar' => 'arc[l-product-id]',
-	'lookUpTable' => array(
-		'table' => 'mshop_text mt JOIN mshop_product_list pl ON pl.refid=mt.id JOIN mshop_text_type mtt ON mtt.id = mt.typeid',
-		'id_field' => 'parentid',
-		'alias_field' => 'content',
-		'addWhereClause' => " AND pl.domain='text' AND mt.status > 0 AND (mt.langid='{TSFE:config|config|language}' or mt.langid is null) AND mt.domain='product' AND mtt.domain='product' AND mtt.code='name'",
-		'useUniqueCache' => 1,
-		'useUniqueCache_conf' => array(
-			'strtolower' => 1,
-			'spaceCharacter' => '-',
-		),
-		'insertData' => 1,
-	),
-);
+// This creates a cHash mismatch so pages are not fetched from cache
+//$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['fileName']['defaultToHTMLsuffixOnPrev'] = '';
+
 
 $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['arcavias'] = array(
 	array(
@@ -48,44 +26,47 @@ $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['a
 	),
 );
 
-$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['catalog'] = array(
-	array(
-		'GETvar' => 'arc[l-sort]',
-		'valueMap' => array(
-			'name' => 'name',
-			'price' => 'price',
-		),
-		'noMatch' => 'bypass',
-	),
-	array(
-		'GETvar' => 'arc[l-page]',
-	),
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['fixedPostVars']['_DEFAULT'][] = array(
+	'GETvar' => 'arc[f-catalog-id]',
 );
 
-$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['basket'] = array(
-	array(
-		'GETvar' => 'arc[b-action]'
-	),
-	array(
-		'GETvar' => 'arc[b-position]'
-	),
-	array(
-		'GETvar' => 'arc[b-quantity]'
-	),
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['fixedPostVars']['_DEFAULT'][] = array(
+	'GETvar' => 'arc[l-product-id]',
 );
 
-$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['checkout'] = array(
-	array(
-		'GETvar' => 'arc[c-step]',
-		'valueMap' => array(
-			'address' => 'address',
-			'delivery' => 'delivery',
-			'payment' => 'payment',
-			'summary' => 'summary',
-			'order' => 'order',
-		),
-		'noMatch' => 'bypass',
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['c'][] = array(
+	'GETvar' => 'arc[l-page]',
+);
+
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['c'][] = array(
+	'GETvar' => 'arc[l-sort]',
+	'valueMap' => array(
+		'name' => 'name',
+		'price' => 'price',
 	),
+	'noMatch' => 'bypass',
+);
+
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['c'][] = array(
+	'GETvar' => 'arc[f-search-text]',
+);
+
+
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['b'][] = array( 'GETvar' => 'arc[b-action]' );
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['b'][] = array( 'GETvar' => 'arc[b-position]' );
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['b'][] = array( 'GETvar' => 'arc[b-quantity]' );
+
+
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['o'][] = array(
+	'GETvar' => 'arc[c-step]',
+	'valueMap' => array(
+		'address' => 'address',
+		'delivery' => 'delivery',
+		'payment' => 'payment',
+		'summary' => 'summary',
+		'order' => 'order',
+	),
+	'noMatch' => 'bypass',
 );
 
 ?>
