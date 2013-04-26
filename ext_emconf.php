457e992cc084c85b751c77d12a31ad0fcd80327f
Norbert <norbert@linuxnetworks.de>
2013-04-26 17:41:27 +0200

    Adds sr_feuser_register as required TYPO3 extension (for TYPO3 customer manager)

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,6 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'typo3' => '4.5.0-4.99.99',
 			'extbase' => '1.3.2-4.99.99',
 			'scheduler' => '1.1.0-4.99.99',
+			'sr_feuser_register' => '2.6.3-3.99.99'
 		),
 		'conflicts' => array(
 		),
