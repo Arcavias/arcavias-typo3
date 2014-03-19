d407ccde91c8ef51da29d1876c3ca3c951feba06
Norbert <n.sendetzky@metaways.de>
2014-03-19 16:18:18 +0100

    Removes autoloading for files that are automatically found by Extbase

--- ext_autoload.php
+++ ext_autoload.php
@@ -4,15 +4,6 @@
 $extensionPath = t3lib_extMgm::extPath( 'arcavias' );
 
 return array(
-	'tx_arcavias_base' => $extensionPath . 'Classes/Base.php',
-
-	'tx_arcavias_controller_abstract' => $extensionPath . 'Classes/Controller/Abstract.php',
-	'tx_arcavias_controller_accountcontroller' => $extensionPath . 'Classes/Controller/AccountController.php',
-	'tx_arcavias_controller_admincontroller' => $extensionPath . 'Classes/Controller/AdminController.php',
-	'tx_arcavias_controller_basketcontroller' => $extensionPath . 'Classes/Controller/BasketController.php',
-	'tx_arcavias_controller_catalogcontroller' => $extensionPath . 'Classes/Controller/CatalogController.php',
-	'tx_arcavias_controller_checkoutcontroller' => $extensionPath . 'Classes/Controller/CheckoutController.php',
-
 	'tx_arcavias_custom_realurl' => $extensionPath . 'Classes/Custom/Realurl.php',
 	'tx_arcavias_custom_wizicon' => $extensionPath . 'Classes/Custom/Wizicon.php',
 
@@ -23,8 +14,6 @@ return array(
 	'tx_arcavias_scheduler_task_typo4' => $extensionPath . 'Classes/Scheduler/Task/Typo4.php',
 	'tx_arcavias_scheduler_provider_typo4' => $extensionPath . 'Classes/Scheduler/Provider/Typo4.php',
 	'tx_arcavias_scheduler_provider_abstract' => $extensionPath . 'Classes/Scheduler/Provider/Abstract.php',
-	'Arcavias\Arcavias\Scheduler\Task\Typo6' => $extensionPath . 'Classes/Scheduler/Task/Typo6.php',
-	'Arcavias\Arcavias\Scheduler\Provider\Typo6' => $extensionPath . 'Classes/Scheduler/Provider/Typo6.php',
 );
 
 ?>
